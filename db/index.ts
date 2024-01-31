879d79c1463dca99b103e106966c650b1c76b47b
Luke Davies <47165253+LukeDavies12@users.noreply.github.com>
2024-01-31 17:23:54 -0600

    maybe

--- db/index.ts
+++ db/index.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from "@prisma/client";
 declare global {
   // allow global `var` declarations
   // eslint-disable-next-line no-var
-  var db: PrismaClient | undefined;
+  var db: PrismaClient;
 }
 
 export const db =
@@ -11,3 +11,11 @@ export const db =
   new PrismaClient({
     log: ["query"],
   });
+
+if (process.env.NODE_ENV === "development") {
+  global.db = db;
+}
+
+if (process.env.NODE_ENV === "production") {
+  db.$connect();
+}
