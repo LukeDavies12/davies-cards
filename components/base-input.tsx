64a993577a5ee107f92cd6a0b42077cf9cce82e6
Luke Davies <47165253+LukeDavies12@users.noreply.github.com>
2025-12-02 19:56:37 -0600

    looking to keep modal from zooming in when focusing an input on phone

--- components/base-input.tsx
+++ components/base-input.tsx
@@ -5,7 +5,7 @@ interface BaseInputProps extends InputHTMLAttributes<HTMLInputElement> { }
 export default function BaseInput({ className = '', ...props }: BaseInputProps) {
   return (
     <input
-      className={`w-full px-2 py-1 text-sm bg-neutral-100 rounded-md hover:bg-neutral-200 focus:bg-neutral-300 focus:outline-none transition-colors ease-linear duration-100 border border-transparent ${className}`}
+      className={`w-full px-2 py-1 text-base bg-neutral-100 rounded-md hover:bg-neutral-200 focus:bg-neutral-300 focus:outline-none transition-colors ease-linear duration-100 border border-transparent ${className}`}
       {...props}
     />
   );
